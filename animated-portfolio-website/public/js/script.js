179f3ff3d03c587d5193d48c23c932b805759728
Safak <safakkel1903@gmail.com>
2023-04-06 20:05:31 +0300

    updated

---
+++ animated-portfolio-website/public/js/script.js
@@ -0,0 +1,7 @@
+let typed = new Typed("#multiple-text", {
+    strings: ["Frontend Developer", "Programmer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
--- animated-portfolio-website/tailwind.config.js
+++ animated-portfolio-website/tailwind.config.js
@@ -23,6 +23,9 @@ module.exports = {
       animation : {
         "slideRight": 'slideRight 1s ease forwards',
         "slideTop": 'slideTop .5s ease forwards',
+        "slideBottom": 'slideBottom 1s ease forwards',
+        "slideLeft": 'slideLeft 1s ease forwards',
+        "zoomIn": 'zoomIn 1s ease forwards, floatImage 4s ease-in-out infinite',
       },
       keyframes: {
         slideRight: {
@@ -30,8 +33,25 @@ module.exports = {
           '100%': {transform: 'translateX(0)', opacity: '1'},
         },
         slideTop: {
+          '0%': {transform: 'translateY(100px)', opacity: '0'},
+          '100%': {transform: 'translateY(0)', opacity: '1'},
+        },
+        slideBottom: {
           '0%': {transform: 'translateY(-100px)', opacity: '0'},
           '100%': {transform: 'translateY(0)', opacity: '1'},
+        },
+        slideLeft: {
+          '0%': {transform: 'translateX(100px)', opacity: '0'},
+          '100%': {transform: 'translateY(0)', opacity: '1'},
+        },
+        zoomIn: {
+          '0%': {transform: 'scale(0)', opacity: '0'},
+          '100%': {transform: 'scale(1)', opacity: '1'},
+        },
+        floatImage: {
+          '0%': {transform: 'translateY(0)'},
+          '50%': {transform: 'translateY(-24px)'},
+          '100%': {transform: 'translateY(0)'},
         },
       },
     },
